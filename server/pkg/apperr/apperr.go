54fcb4f338b94e09f60f09ad315c0c0ddf7e6cbc
Ademun <78663343+Ademun@users.noreply.github.com>
2026-09-17 00:21:03 +0300

    dev: changed apperr.Err implementation. Separated http code from err code.

--- server/pkg/apperr/apperr.go
+++ server/pkg/apperr/apperr.go
@@ -1,21 +1,41 @@
 package apperr
 
-import (
-	"fmt"
+import "fmt"
+
+type Kind string
+
+const (
+	KindNotFound     Kind = "not_found"
+	KindValidation   Kind = "validation"
+	KindForbidden    Kind = "forbidden"
+	KindUnauthorized Kind = "unauthorized"
+	KindConflict     Kind = "conflict"
+	KindInternal     Kind = "internal"
 )
 
-type ErrCode int
-
 type Err struct {
-	Code int
+	Kind Kind
+	Code string
 	Msg  string
 	Err  error
 }
 
-func New(code int, msg string, err error) *Err {
-	return &Err{Code: code, Msg: msg, Err: err}
+func New(kind Kind, code, msg string, err error) *Err {
+	return &Err{Kind: kind, Code: code, Msg: msg, Err: err}
 }
 
-func (e Err) Error() string {
-	return fmt.Sprintf("Code %d: %s. Err: %s", e.Code, e.Msg, e.Err.Error())
+func (e *Err) Error() string {
+	if e.Err == nil {
+		return fmt.Sprintf("%s: %s", e.Code, e.Msg)
+	}
+	return fmt.Sprintf("%s: %s: %s", e.Code, e.Msg, e.Err)
 }
+
+func (e *Err) Unwrap() error {
+	return e.Err
+}
+
+func NotFound(code, msg string, err error) *Err   { return New(KindNotFound, code, msg, err) }
+func Forbidden(code, msg string, err error) *Err  { return New(KindForbidden, code, msg, err) }
+func Validation(code, msg string, err error) *Err { return New(KindValidation, code, msg, err) }
+func Internal(code, msg string, err error) *Err   { return New(KindInternal, code, msg, err) }
